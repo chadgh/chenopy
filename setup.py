c45955cbb445a5143dd10a39f62100cf1d6470c8
chadgh <chadgh@gmail.com>
2014-01-14 14:08:35 -0700

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='Chenopy',
+      version='1.0',
+      description='Chess notation parsing tools.',
+      author='Chad G. Hansen',
+      author_email='chadgh@gmail.com',
+      url='https://github.com/chadgh/chenopy',
+      packages=['chenopy', 'chenopy.tests'],
+      license='Creative Commons Attribution-Noncommercial-Share Alike license',
+      long_description=open('README.md').read(),
+      tests_require=['coverage==3.7.1', 'nose==1.3.0'])
